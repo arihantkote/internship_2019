7c37eb94f97be8a6937343e425f965c0562e60ee
arihantkote <arihantkote1070@gmail.com>
2019-05-30 13:10:49 +0530

    user input table  with comments using javascript

---
+++ internship_2019/day9/jsc/table.js
@@ -0,0 +1,19 @@
+//logic of tables and for synatx
+
+
+function result(){
+var i ,num,
+
+
+ num=Number(document.getElementById('enter').value);
+
+
+    for (i=1 ;i<=10 ;i++)
+    {
+        
+        var ans= document.getElementById('answer')
+        ans.innerHTML += (num*i) + "<br/>" 
+        
+    }
+
+}
