dadf7964232047d25f4a4f1edad1e9fcc0f65d24
arihantkote <arihantkote1070@gmail.com>
2019-05-30 12:14:11 +0530

    alert function is used

--- internship_2019/day9/jsc/evenodd.js
+++ internship_2019/day9/jsc/evenodd.js
@@ -1,5 +1,6 @@
  
 // logic of even  and odd 
+
             function findOddEven(){
                 
                 var num = document.getElementById('num').value;
@@ -7,7 +8,9 @@
                 if ( num % 2 == 0)
                  {
                     document.getElementById('result').innerHTML=num + ' is a Even number';
+                    alert( value=num +" is even number")
                 }else{
                     document.getElementById('result').innerHTML =num + ' is a Odd number';
+                    alert( value=num+" is odd number")
                 }
                 } 
