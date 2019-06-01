f336c21d32cc34796cc57a43fa7206d41fa2860f
arihantkote <arihantkote1070@gmail.com>
2019-06-01 11:08:41 +0530

    day and time code uusing java-script

--- internship_2019/day9/jsc/table.js
+++ internship_2019/day9/jsc/table.js
@@ -2,18 +2,22 @@
 
 
 function result(){
-var i ,num,
+var num,i ;
+
 
 
  num=Number(document.getElementById('enter').value);
 
-
-    for (i=1 ;i<=10 ;i++)
+    for ( i=1 ;i<=10 ;i++)
     {
         
         var ans= document.getElementById('answer')
-        ans.innerHTML += (num*i) + "<br/>" 
+        ans .innerHTML += num + "*"+ i + "=" + (num*i) + "<br>"
+    
+
         
     }
+
+    
 
 }
