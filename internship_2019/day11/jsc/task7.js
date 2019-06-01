2446737e6df3d6a9f0b00f1d91b23115dd69a19b
arihantkote <arihantkote1070@gmail.com>
2019-06-01 16:03:59 +0530

    MULTIPILCATION AND DIVISION

--- internship_2019/day11/jsc/task7.js
+++ internship_2019/day11/jsc/task7.js
@@ -4,18 +4,18 @@ function multiplication() {
     a = Number(document.getElementById("first").value);
     b = Number(document.getElementById("second").value);
     c = a * b;
-    document.getElementById("answer").value = c;
+    document.getElementById("answer").innerHTML+= c;
 }
 function division() {
     var a, b, c;
     a = Number(document.getElementById("first").value);
     b = Number(document.getElementById("second").value);
     c = a / b;
-    document.getElementById("answer").value = c;
+    document.getElementById("answer").innerHTML += c;
 }
  function onenum(){
     var onenum;
     onenum = Number(document.getElementById("onenum").value)
-        document.getElementById("first").value.getElementById("onenum");
+    onenum.document.getElementById("first").value.getElementById("onenum");
     }
  
