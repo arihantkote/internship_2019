60c44662a1c89c911a1e06aca106ea73c51bf68a
arihantkote <arihantkote1070@gmail.com>
2019-06-01 15:45:24 +0530

    string rotation using java script

--- internship_2019/day11/jsc/task6.js
+++ internship_2019/day11/jsc/task6.js
@@ -4,6 +4,6 @@ for (var year = 2014; year <= 2050; year++)
     {
     var d = new Date(year, 0, 1);
     if ( d.getDay() === 0 )
-        document.getElementById("list").value="1st January is being a Sunday"+year;
+        document.getElementById("list").innerHTML+="1st January is being a Sunday"+year+"<br>";
     }
 }
